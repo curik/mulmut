94067b6f5a660a8efb8665f9cefc0b2e10982900
curik <ayohan86@gmail.com>
2015-06-15 17:40:49 +0700

    working on verify. now can approve or reject orders

--- verify/verify.js
+++ verify/verify.js
@@ -30,17 +30,7 @@
 
         });
 
-        $scope.verifyOrder = function(status){
-            console.log(status);
-            $scope.request.update({status: status})
-            var onComplete = function(error) {
-                if (error) {
-                    console.log('Synchronization failed');
-                } else {
-                    console.log('Synchronization succeeded');
-                }
-            }
-        };
+        
         /*
         $scope.$watch('profileEmail', function() {
             $scope.admin.email = $scope.profileEmail;
@@ -76,6 +66,11 @@
         }
         */
 
+        $scope.verifyOrder = function(){
+            verify.verifyOrder($scope.admin.orderStatus,orderRef);
+
+        };
+
         $scope.$on('$destroy', function () {
             $scope.request.$destroy();
             //$scope.vendor.$destroy();
@@ -91,48 +86,14 @@
         function($firebase, $location, $rootScope, FBURL) {
             var factory = {};
 
-
             factory.ref = new Firebase(FBURL);
             factory.ordersRef = factory.ref.child("orders");
-
-            var authData = factory.ref.getAuth();
-            factory.vendorRef = factory.ref.child("vendors/" + authData.uid);
-            //factory.vendorCurrentBalanceRef = factory.ref.child("vendorsInfo/" + authData.uid + "/balance/currentBalance");
-            //factory.vendoradminsCountRef = factory.ref.child("vendorsInfo/" + authData.uid + "/metrics/adminsCount");
-
-            /*
-            factory.getBalance = function() {
-                factory.vendorCurrentBalanceRef.on("value", function(snapshot) {
-                    if(angular.isDefined(snapshot) && snapshot && snapshot.val() > 0) {
-                        $rootScope.myBalance = snapshot.val();
-                        return snapshot.val();
-                    } else {
-                        return 0;
-                    }
-                }, function (errorObject) {
-                    console.log("Read failed: " + errorObject.code);
-                });
+            
+            factory.verifyOrder = function(newstatus,orderRef){
+                alert("Order " + newstatus);
+                factory.orderRef = factory.ref.child("orders/" + orderRef);
+                factory.orderRef.update({status : newstatus});
             };
-
-            factory.placeverify = function(admin, orderRef) {
-                var self = this;
-                var adminRef = factory.ref.child("orders/" + orderRef + "/admins");
-
-                //TODO: error message of admins does not exist
-                adminRef.push(admin);
-                factory.vendorRef.child("admins").push(admin);
-/*
-                factory.vendoradminsCountRef.transaction(function (current_value) {
-                    return (current_value || 0) + 1;
-                });
-
-                factory.vendorCurrentBalanceRef.transaction(function (current_value) {
-                    return (current_value || 0) - 5000;
-                });
-
-                $location.path("/admin");
-            };
-            */
 
             return factory;
         }
@@ -140,7 +101,7 @@
 
     app.config(['$routeProvider', function($routeProvider) {
         $routeProvider.whenAuthenticated('/verify/:orderRef', {
-            templateUrl: '/verify/verify.html',
+            templateUrl: 'verify/verify.html',
             controller: 'VerifyCtrl'
         });
     }]);
